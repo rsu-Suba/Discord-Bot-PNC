321cfa05deaa0167df805d3cdcac069f778ea231
rsu-Suba <suba1105usui@gmail.com>
2023-06-11 10:33:19 +0900

    Fix index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const token = process.env['token'];
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const client = new Client({intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
 
 client.on('ready', () => {
-	console.log(`${client.user.tag} logs in.`);
+  console.log(`${client.user.tag} logs in.`);
 });
 
 client.login(token);
@@ -17,29 +18,29 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`No data or execute to do ${filePath}`);
-	}
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`No data or execute to do ${filePath}`);
+  }
 }
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`Not found ${interaction.commandName}`);
-		return;
-	}
+  if (!command) {
+    console.error(`Not found ${interaction.commandName}`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'Error', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'Error', ephemeral: true });
+  }
 });
